ed5000969461d31d5bb8745f2a857f0c576bbb11
JOSE RICARDO <jricardo.morais@hotmail.com>
2025-02-07 16:21:06 -0300

    Add project files.

---
+++ DetectCapital/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        // Exemplos de entrada
+        string[] words = { "USA", "FlaG", "leetcode", "Google" };
+
+        foreach (var word in words)
+        {
+            bool result = DetectCapitalUse(word);
+            Console.WriteLine($"Input: {word}");
+            Console.WriteLine($"Output: {result}\n");
+        }
+    }
+
+    public static bool DetectCapitalUse(string word)
+    {
+        // Verifica se todas as letras são maiúsculas
+        if (word.ToUpper() == word)
+            return true;
+
+        // Verifica se todas as letras são minúsculas
+        if (word.ToLower() == word)
+            return true;
+
+        // Verifica se somente a primeira letra é maiúscula
+        if (char.IsUpper(word[0]) && word.Substring(1).ToLower() == word.Substring(1))
+            return true;
+
+        return false;
+    }
+}
